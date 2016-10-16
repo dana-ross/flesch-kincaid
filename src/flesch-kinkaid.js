1caf0deb75a3c9c5ad90610c5bc4fa829e6caed1
Dave Ross <dave@davidmichaelross.com>
2016-10-15 20:51:39 -0400

    Removed test functions

--- src/flesch-kinkaid.js
+++ src/flesch-kinkaid.js
@@ -66,21 +66,3 @@ const wordsPerSentence = x => words(x) / sentences(x)
 
 const rate = x => 206.835 - 1.015 * wordsPerSentence(x) - 84.6 * syllablesPerWord(x)
 const grade = x => 0.39 * wordsPerSentence(x) + 11.8 * syllablesPerWord(x) - 15.59
-const test = (fn, x, y) => console.log(x + (Math.round(fn(x)) === Math.round(y) ? 'Pass' : 'Fail (' + fn(x) + ')'))
-
-test(syllables, "The cat sat on the mat.", 6);
-test(words, "The cat sat on the mat.", 6);
-test(sentences, "The cat sat on the mat.", 1);
-test(rate, "The cat sat on the mat.", 116);
-
-test(words, "This sentence, taken as a reading passage unto itself, is being used to prove a point.", 16);
-test(syllables, "This sentence, taken as a reading passage unto itself, is being used to prove a point.", 23);
-test(rate, "This sentence, taken as a reading passage unto itself, is being used to prove a point.", 74.1);
-
-test(words, "The Australian platypus is seemingly a hybrid of a mammal and reptilian creature.", 13);
-test(syllables, "The Australian platypus is seemingly a hybrid of a mammal and reptilian creature.", 25);
-test(rate, "The Australian platypus is seemingly a hybrid of a mammal and reptilian creature.", 37);
-test(grade, "The Australian platypus is seemingly a hybrid of a mammal and reptilian creature.", 13);
-
-test(syllables, 'agreeable', 4)
-test(syllables, 'belguim', 2)
